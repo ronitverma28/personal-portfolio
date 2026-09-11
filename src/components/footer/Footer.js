7869681ff8375b09e4170c3744e72e9e7663ab67
ronitverma28 <ronitroy22678@gmail.com>
2026-09-11 22:10:24 +0530

    footer updated

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,17 +1,22 @@
 import React from "react";
+
 import "./Footer.css";
+
 import { Fade } from "react-reveal";
+
 import { greeting } from "../../portfolio.js";
 
 export default function Footer(props) {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div className="footer-div">
+    <footer className="footer-div">
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
-          Made with <span role="img" aria-label="heart">&#10084;&#65039;</span> by {greeting.title}
+          <span className="copyright-symbol">&copy;</span> {currentYear}{" "}
+          {greeting.title} • Designed & Developed with Passion
         </p>
-
       </Fade>
-    </div>
+    </footer>
   );
 }
